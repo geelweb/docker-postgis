d81a0574469bf997b104600e4ed6cf0c09605328
Guillaume Luchet <guillaume@geelweb.org>
2015-07-27 23:10:34 +0200

    do not start / stop postgres server

--- init-postgis.sh
+++ init-postgis.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-# At this point the database was created by the postgres image entrypoint, but
-# the server is not yet running.
-#
-# As we can not create extension in single-user mode we have to start the
-# server. (the -w option means "wait until operation completes")
-gosu postgres pg_ctl start -w
-
 # Enable PostGIS as describe here http://postgis.net/install/
 
 # enable PostGIS
@@ -21,8 +14,4 @@ gosu postgres psql -U postgres $POSTGRES_DB -c "create extension fuzzystrmatch"
 
 # Enable US Tiger Geocoder
 gosu postgres psql -U postgres $POSTGRES_DB -c "create extension postgis_tiger_geocoder"
-
-# We stop the server, the postgres image entrypoint script will continue and
-# will finish by starting it.
-gosu postgres pg_ctl stop -w
 
