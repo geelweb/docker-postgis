978aa72e26bb2bf64b6f90211e084763cb054b07
Guillaume Luchet <gluchet@gmail.com>
2017-10-31 11:02:49 +0100

    Add quotes around postgres user and db

--- 9.3/init-postgis.sh
+++ 9.3/init-postgis.sh
@@ -6,15 +6,15 @@ export PGPASSWORD="$POSTGRES_PASSWORD"
 # Enable PostGIS as describe here http://postgis.net/install/
 
 # enable PostGIS
-psql -U $POSTGRES_USER $POSTGRES_DB -c "create extension postgis"
+psql -U "$POSTGRES_USER" "$POSTGRES_DB" -c "create extension postgis"
 
 # enable Topology
-psql -U $POSTGRES_USER $POSTGRES_DB -c "create extension postgis_topology"
+psql -U "$POSTGRES_USER" "$POSTGRES_DB" -c "create extension postgis_topology"
 
 # fuzzy matching needed for Tiger
-psql -U $POSTGRES_USER $POSTGRES_DB -c "create extension fuzzystrmatch"
+psql -U "$POSTGRES_USER" "$POSTGRES_DB" -c "create extension fuzzystrmatch"
 
 # Enable US Tiger Geocoder
-psql -U $POSTGRES_USER $POSTGRES_DB -c "create extension postgis_tiger_geocoder"
+psql -U "$POSTGRES_USER" "$POSTGRES_DB" -c "create extension postgis_tiger_geocoder"
 
 
