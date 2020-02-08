08719b3e5d4d076e73a5b13317452a14f66f612b
Guillaume Luchet <gluchet@gmail.com>
2020-02-08 09:40:43 +0100

    Add postgres12 image

---
+++ 12/init-postgis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+export PGPASSWORD="$POSTGRES_PASSWORD"
+
+# Enable PostGIS as describe here http://postgis.net/install/
+
+# enable PostGIS
+psql -U "$POSTGRES_USER" "$POSTGRES_DB" -c "create extension postgis"
+
+# enable Topology
+psql -U "$POSTGRES_USER" "$POSTGRES_DB" -c "create extension postgis_topology"
+
+# fuzzy matching needed for Tiger
+psql -U "$POSTGRES_USER" "$POSTGRES_DB" -c "create extension fuzzystrmatch"
+
+# Enable US Tiger Geocoder
+psql -U "$POSTGRES_USER" "$POSTGRES_DB" -c "create extension postgis_tiger_geocoder"
+
+
